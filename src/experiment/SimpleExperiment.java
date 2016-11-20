00a8d551630552b1140e86636e5e74b306291280
Peter Pilgerstorfer <peter.pilgerstorfer@outlook.com>
2016-11-20 17:46:05 +0100

    Added squared distance cost function and the possibility to load a vector from a file

--- src/experiment/SimpleExperiment.java
+++ src/experiment/SimpleExperiment.java
@@ -9,17 +9,25 @@ import agent.logging.JFreeChartLogger;
 import agent.dataset.Dataset;
 import agent.*;
 import agent.dataset.GaussianDataset;
+import agent.logging.GraphLogger;
+import agent.logging.MovieLogger;
 import data.Plan;
 import data.Vector;
+import data.io.VectorIO;
 import dsutil.generic.RankPriority;
 import dsutil.protopeer.services.topology.trees.DescriptorType;
 import dsutil.protopeer.services.topology.trees.TreeType;
 import func.DifferentiableCostFunction;
 import func.IndexCostFunction;
 import func.PlanCostFunction;
+import func.SqrDistCostFunction;
 import func.VarCostFunction;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import protopeer.Experiment;
 import protopeer.Peer;
 import protopeer.PeerFactory;
@@ -38,23 +46,30 @@ import util.TreeArchitecture;
  * @author Peter
  */
 public class SimpleExperiment extends SimulatedExperiment {
-
+    
     private final Random random = new Random();
-
+    
     public static void main(String[] args) {
         new SimpleExperiment().run();
     }
-
+    
     public void run() {
         // constants
-        int a = 1000;
+        int a = 127;
         int p = 16;
-        int d = 100;
+        int d = 40;
         int c = 2;
-        int t = 100;
-        int runs = 5;
+        int t = 20;
+        int runs = 1;
         double lambda = 5;
-        DifferentiableCostFunction globalCostFunc = new VarCostFunction();
+        //DifferentiableCostFunction globalCostFunc = new VarCostFunction();
+        DifferentiableCostFunction globalCostFunc;
+        try {
+            globalCostFunc = new SqrDistCostFunction(VectorIO.readVector(new File("input-data/sample_vector.txt")));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SimpleExperiment.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
         PlanCostFunction localCostFunc = new IndexCostFunction();
 
         // loggers
@@ -64,11 +79,12 @@ public class SimpleExperiment extends SimulatedExperiment {
         //loggingProvider.add(new LocalCostLogger());
         loggingProvider.add(new TerminationLogger());
         loggingProvider.add(new JFreeChartLogger());
+        loggingProvider.add(new MovieLogger());
+        //loggingProvider.add(new GraphLogger<>(GraphLogger.Type.Change, null));
         //loggingProvider.add(new FileWriter("simple.log"));
 
         // dataset
         //Dataset<Vector> dataset = new GaussianDataset(p, d, 0, 1, new Random(0));
-        
         // network
         TreeArchitecture architecture = new TreeArchitecture();
         architecture.balance = BalanceType.WEIGHT_BALANCED;
@@ -77,23 +93,23 @@ public class SimpleExperiment extends SimulatedExperiment {
         architecture.rank = DescriptorType.RANK;
         architecture.rankGenerator = (idx, agent) -> (double) idx;
         architecture.type = TreeType.SORTED_HtL;
-
+        
         for (int r = 0; r < runs; r++) {
             final int run = r;
             
             random.setSeed(r);
             Dataset<Vector> dataset = new GaussianDataset(p, d, 0, 1, new Random(random.nextLong()));
-
+            
             SimpleExperiment.initEnvironment();
             init();
-
+            
             PeerFactory peerFactory = new PeerFactory() {
-
+                
                 @Override
                 public Peer createPeer(int peerIndex, Experiment e) {
                     List<Plan<Vector>> possiblePlans = dataset.getPlans(peerIndex);
                     AgentLoggingProvider agentLP = loggingProvider.getAgentLoggingProvider(peerIndex, run);
-
+                    
                     IeposAgent newAgent = new IeposAgent(t, possiblePlans, globalCostFunc, localCostFunc, agentLP, random.nextLong());
                     //newAgent.setLambda(lambda);
                     //newAgent.setPlanSelector(new IeposIndividualGradientPlanSelector());
@@ -101,18 +117,18 @@ public class SimpleExperiment extends SimulatedExperiment {
                     //Agent newAgent = new EposAgent(possiblePlans, globalCostFunc, localCostFunc, agentLP, random.nextLong());
                     //Agent newAgent = new BestStepAgent(t, possiblePlans, globalCostFunc, localCostFunc, agentLP, random.nextLong());
                     Peer newPeer = new Peer(peerIndex);
-
+                    
                     architecture.addPeerlets(newPeer, newAgent, peerIndex, a);
-
+                    
                     return newPeer;
                 }
             };
             initPeers(0, a, peerFactory);
             startPeers(0, a);
-
+            
             runSimulation(Time.inSeconds(3 + t));
         }
-
+        
         loggingProvider.print();
     }
 }
