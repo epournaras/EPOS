338de507b64518f42c6fb8682b3e999a7c503b9f
AmalAldawsari <amalaldawsari1991@gmail.com>
2023-01-06 12:08:31 +0000

    Behaviours added

--- src/main/java/agent/Agent.java
+++ src/main/java/agent/Agent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package agent;
+package src.main.java.agent;
 
 import data.Plan;
 import func.CostFunction;
